efc329c79eaff5519d52c3e2e4f9c1b90206e36b
JohnMorales15 <119957569+JohnMorales15@users.noreply.github.com>
2023-02-08 09:24:57 -0700

    added unique ID

--- src/main/java/org/launchcode/codingevents/models/Events.java
+++ src/main/java/org/launchcode/codingevents/models/Events.java
@@ -1,12 +1,18 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Events {
     private String name;
     private String description;
+    private int id;
+    private static int nextId = 1;
 
     public Events(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -25,9 +31,25 @@ public class Events {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Events events = (Events) o;
+        return id == events.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
